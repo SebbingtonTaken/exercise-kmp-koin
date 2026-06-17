748768a4c92e5dd5d609814476ceb235308e4016
SebbingtonTaken <128566685+SebbingtonTaken@users.noreply.github.com>
2026-06-16 19:39:08 -0600

    dsafsda
    asdf

--- KMPKoin/shared/src/commonMain/kotlin/io/github/kevinah95/kmpkoin/data/UserRepository.kt
+++ KMPKoin/shared/src/commonMain/kotlin/io/github/kevinah95/kmpkoin/data/UserRepository.kt
@@ -1,7 +1,7 @@
 package io.github.kevinah95.kmpkoin.data
 
 class UserRepository {
-    private val users = mutableListOf<User>()
+    private val users = mutableListOf<`User.kt`>()
 
     fun addUser(user: User) {
         users.add(user)
---
+++ KMPKoin/shared/src/commonMain/kotlin/io/github/kevinah95/kmpkoin/di/DataModule.kt
@@ -0,0 +1,10 @@
+
+
+package io.github.kevinah95.kmpkoin.di
+
+import io.github.kevinah95.kmpkoin.data.UserRepository
+import org.koin.dsl.module
+
+val dataModule = module {
+    single { UserRepository() }
+}
